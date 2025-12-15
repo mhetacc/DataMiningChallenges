20c85da37ebac53b138237de3d1d66d206dd45e4
mhetac <marco.bello.vi@gmail.com>
2025-12-15 14:07:06 +0100

    report: pca graph

--- RiceChallenge/rice_challenge.r
+++ RiceChallenge/rice_challenge.r
@@ -8,3 +8,15 @@ fit = lm(Class ~ ., data=train)
 # vector with prediction for each row in test dataframe
 yhat = (predict(fit, newdata=test)>1.5)+1
 
+# principal components test dataset
+pc <- prcomp(test, center = TRUE, scale. = TRUE)
+
+# plot two principal components with colouring the predicted class with yhat
+library(ggplot2)
+
+pc_df <- as.data.frame(pc$x[, 1:2])
+pc_df$PredictedClass <- factor(yhat)
+
+ggplot(pc_df, aes(PC1, PC2, color = PredictedClass)) +
+  geom_point() +
+  labs(title = "PCA: coloured by classess predicted using lm()")
